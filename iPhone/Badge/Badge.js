6bfd8c750e00303a17386c9653408a279da5cb15
Greg Allen <mail@jga23.com>
2010-12-13 18:34:58 -0800

    check if phonegap exists before loading plugin

--- iPhone/Badge/Badge.js
+++ iPhone/Badge/Badge.js
@@ -10,28 +10,30 @@
  * to add a number (with a red background) to each icon
  * @constructor
  */
-function Badge() {
-}
-
-/**
- * Positive integer sets the badge, 0 or negative clears it
- */
-Badge.prototype.set = function(options) {
-    PhoneGap.exec("Badge.setBadge", options);
-};
-
-/**
- * Shorthand to set the badge to 0
- */
-Badge.prototype.clear = function() {
-    PhoneGap.exec("Badge.setBadge", 0);
-};
-
-PhoneGap.addConstructor(function() 
-{
-	if(!window.plugins)
-	{
-		window.plugins = {};
+if (typeof PhoneGap !== "undefined") {
+	function Badge() {
 	}
-    window.plugins.badge = new Badge();
-});
+
+	/**
+	 * Positive integer sets the badge, 0 or negative clears it
+	 */
+	Badge.prototype.set = function(options) {
+		PhoneGap.exec("Badge.setBadge", options);
+	};
+
+	/**
+	 * Shorthand to set the badge to 0
+	 */
+	Badge.prototype.clear = function() {
+		PhoneGap.exec("Badge.setBadge", 0);
+	};
+
+	PhoneGap.addConstructor(function() 
+	{
+		if(!window.plugins)
+		{
+			window.plugins = {};
+		}
+		window.plugins.badge = new Badge();
+	});
+}
